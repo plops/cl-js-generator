bcacadc780182b2a474bbdefcfdbd8f156a7b541
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-09-29 21:55:12 +0200

    npm start works
    martin@labolg ~/stage/cl-js-generator/example/02_electron $ npm start
    
    > 02_electron@1.0.0 start /home/martin/stage/cl-js-generator/example/02_electron
    > electron .
    
    hello from electron

---
+++ example/02_electron/app/main.js
@@ -0,0 +1,5 @@
+const {app}=require("electron")
+;
+;
+app.on("ready", function (){    console.log("hello from electron");
+});
--- example/02_electron/gen00.lisp
+++ example/02_electron/gen00.lisp
@@ -4,13 +4,21 @@
 (in-package :cl-js-generator)
 
 
-;; emerge -av wxpython
+;; bookmark list application from Steve Kinney: Electron in Action p. 18
+;; https://github.com/electron-in-action/bookmarker
 
-
+;; mkdir app
+;; touch app/main.js app/renderer.js app/style.css app/index.html
+;; git add app/main.js app/renderer.js app/style.css app/index.html
+;; npm init
+;; -- answer a bunch of questions
+;; npm install electron --save
+;; -- add start script to package.json
+;; npm start
+;;  sudo npm install -g js-beautify
 (progn
-  (defparameter *repo-sub-path* "01_electron")
+  (defparameter *repo-sub-path* "02_electron")
   (defparameter *path* (format nil "/home/martin/stage/cl-js-generator/example/~a" *repo-sub-path*))
-  (defparameter *code-file* "run_00_show")
   (defparameter *source* (format nil "~a/source/~a" *path* *code-file*))
   (defparameter *inspection-facts*
     `((10 "")))
@@ -20,21 +28,12 @@
       "Sunday"))
 
   
-  (let* ((code
-	  `(let ((player (document.getElementById (string "player"))))
-	     (def logger (message)
-	       (if (== (string "object")
-		       (typeof message))
-		   (incf log.innnerHTML
-			 (+ (? (and JSON
-				    JSON.stringify)
-			       (JSON.stringify message)
-			       message)
-			    (string "<br />")))
-		   (incf log.innnerHTML
-			 (+ message
-			  (string "<br />"))))))))
-    (write-source (format nil "~a/source/~a" *path* *code-file*) code)))
+  (let* ()
+    (write-source (format nil "~a/app/main" *path*)
+		  `(let (("{app}" (require (string "electron")) :type const))
+		     (app.on (string "ready")
+			     (lambda ()
+			       (console.log (string "hello from electron"))))))))
 
 
 
