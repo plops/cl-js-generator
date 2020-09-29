3697dd658bf14bdf5c11485bb7be8a60de5084b0
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-09-29 21:43:22 +0200

    add some code

--- example/02_electron/source/run_00_show.js
+++
@@ -1,9 +0,0 @@
-let player = document.getElementById("player");;
-
-function logger(message) {
-    if ((("object") == (typeof(message)))) {
-        log.innnerHTML += (((((JSON) && (JSON.stringify))) ? (JSON.stringify(message)) : (message)) + ("<br />"));
-    } else {
-        log.innnerHTML += ((message) + ("<br />"));
-    };
-};
