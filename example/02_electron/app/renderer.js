ea076f9caa3af16c80ea0aa5b209cd3300f9af4d
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-09-30 17:19:12 +0200

    some issue
    /home/martin/stage/c…/app/renderer.js:13 Refused to apply inline style because it violates the following Content Security Policy directive: "default-src 'self'". Either the 'unsafe-inline' keyword, a hash ('sha256-uNBSgGPE6MECiaBcF245pLtA6vO+n5zeRRSmuCo9xuc='), or a nonce ('nonce-...') is required to enable inline execution. Note also that 'style-src' was not explicitly set, so 'default-src' is used as a fallback.

--- example/02_electron/app/renderer.js
+++ example/02_electron/app/renderer.js
@@ -1,4 +1,5 @@
 const button = document.querySelector(".alert");;
+const parser = new DOMParser();;
 const links = document.querySelector(".links");;
 const error_message = document.querySelector(".error-message");;
 const new_link_form = document.querySelector(".new-link-form");;
@@ -8,6 +9,18 @@ const clear_storage = document.querySelector(".clear-storage");;
 const clearForm = function() {
     new_link_url = null;
 };;
+const parseResponse = function(text) {
+    return parser.parseFromString(text, "text/html");
+};;
+const findTitle = function(nodes) {
+    return nodes.querySelector("title").innerText;
+};;
+const storeLink = function(title, url) {
+    localStorage.setItem(url, JSON.stringify({
+        title: (title),
+        url: (url)
+    }));
+};;
 new_link_url.addEventListener("keyup", function() {
     new_link_submit.disabled = !new_link_url;
 });
@@ -16,7 +29,9 @@ new_link_form.addEventListener("submit", function(event) {
     const url = new_link_url.value;;
     fetch(url).then(function(response) {
         return response.text();
-    });;
+    }).then(parseResponse).then(findTitle).then(function(title) {
+        storeLink(title, url);
+    }).then(clearForm);;
 });
 button.addEventListener("click", function() {
     alert("bla");
