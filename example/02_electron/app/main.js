dd58c6ce2d1286f0f6e6cd784dded7ed510adf5c
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-09-30 12:12:23 +0200

    i cant figure out how to get rid of the worldsafe security warning

--- example/02_electron/app/main.js
+++ example/02_electron/app/main.js
@@ -6,6 +6,10 @@ let mainWindow = null;;
 app.on("ready", function() {
     console.log("hello from electron");
     console.log(`file://${__dirname}/index.html`);
-    mainWindow = new BrowserWindow;
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            worldSafeExecuteJavaScript: true
+        }
+    });
     mainWindow.webContents.loadURL(`file://${__dirname}/index.html`);
 });
--- example/02_electron/gen00.lisp
+++ example/02_electron/gen00.lisp
@@ -41,8 +41,11 @@
 	 (:html
 	  (:head
 	   (:meta :charset "UTF-8")
+	   ;; https://stackoverflow.com/questions/63427191/security-warning-in-the-console-of-browserwindow-electron-9-2-0
+	   #+nil (:meta :http-equiv "Content-Security-Policy"
+			:content "default-src 'self'; script-src 'self' 'unsafe-inline'; connect-src *")
 	   (:meta :http-equiv "Content-Security-Policy"
-		  :content "default-src 'self'; script-src 'self' 'unsafe-inline'; connect-src *")
+		  :content "script-src 'self' 'unsafe-inline'")
 	   (:meta :name "viewport"
 		  :content "width=device-width,initial-scale=1")
 	   (:title "bookmarker"))
@@ -60,7 +63,7 @@ button.addEventListener('click',()=>{alert(__dirname);});"))))))
 			       (console.log (string "hello from electron"))
 			       (console.log (string-backtick "file://${__dirname}/index.html"))
  
-			       (setf mainWindow (new BrowserWindow))
+			       (setf mainWindow (new (BrowserWindow "{webPreferences: { worldSafeExecuteJavaScript: true }}")))
 			       (mainWindow.webContents.loadURL (string-backtick "file://${__dirname}/index.html"))))))))
 
 
