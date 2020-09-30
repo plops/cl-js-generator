50472b4368945df1de0fa56cb82b19338195305c
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-09-30 12:42:01 +0200

    link style sheet into html

--- example/02_electron/gen00.lisp
+++ example/02_electron/gen00.lisp
@@ -40,6 +40,9 @@
 	(cl-who:htm
 	 (:html
 	  (:head
+	   (:link :rel "stylesheet"
+		  :href "style.css"
+		  :type "text/css")
 	   (:meta :charset "UTF-8")
 	   ;; https://stackoverflow.com/questions/63427191/security-warning-in-the-console-of-browserwindow-electron-9-2-0
 	   (:meta :http-equiv "Content-Security-Policy"
