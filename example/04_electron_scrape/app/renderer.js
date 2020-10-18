9171a6df28f08ce363bf32818ca1f0e5c6cfc5e5
martin <martin@localhost.localdomain>
2020-10-18 19:08:17 +0200

    less code

--- example/04_electron_scrape/app/renderer.js
+++ example/04_electron_scrape/app/renderer.js
@@ -1,42 +1,4 @@
 let webview = document.querySelector("webview");;
-const button = document.querySelector(".alert");;
-const parser = new DOMParser();;
-const links = document.querySelector(".links");;
-const error_message = document.querySelector(".error-message");;
-const new_link_form = document.querySelector(".new-link-form");;
-const new_link_url = document.querySelector(".new-link-url");;
-const new_link_submit = document.querySelector(".new-link-submit");;
-const clear_storage = document.querySelector(".clear-storage");;
-const clearForm = function() {
-    new_link_url.value = null;
-};;
-const parseResponse = function(text) {
-    return parser.parseFromString(text, "text/html");
-};;
-const findTitle = function(nodes) {
-    return nodes.querySelector("title").innerText;
-};;
-const storeLink = function(title, url) {
-    localStorage.setItem(url, JSON.stringify({
-        title: (title),
-        url: (url)
-    }));
-};;
-const getLinks = function() {
-    console.log(`getLinks Object.keys(localStorage)=${Object.keys(localStorage)}`);
-    return Object.keys(localStorage).map(function(key) {
-        JSON.parse(localStorage.getItem(key));
-    });
-};;
-const convertToElement = function(link) {
-    console.log(`convertToElement ${link}`);
-    return `<div class="link"></div>`;
-};;
-const renderLinks = function() {
-    console.log(`renderLinks`);
-    const linkElements = getLinks().map(convertToElement).join("");;
-    links.innerHTML = linkElements;;
-};;
 webview.addEventListener("dom-ready", function() {
     let currenURL = webview.getURL();;
     let title = webview.getTitle();;
@@ -47,20 +9,4 @@ return new Promise((resolve,reject)=>{resolve(document.documentElement.innerHTML
 gethtml();`).then(function(html) {
         console.log(html);
     });;
-});
-new_link_url.addEventListener("keyup", function() {
-    new_link_submit.disabled = !new_link_url;
-});
-new_link_form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    const url = new_link_url.value;;
-    fetch(url).then(function(response) {
-        return response.text();
-    }).then(parseResponse).then(findTitle).then(function(title) {
-        storeLink(title, url);
-    }).then(clearForm).then(renderLinks);;
-});
-renderLinks();
-button.addEventListener("click", function() {
-    alert("bla");
 });
