9a8cfa00a68997d6722c214b8a775eb351a8e978
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-07-09 10:31:33 +0200

    esbuild seems to work

--- example/08_ol/source/setup00_esbuild.sh
+++ example/08_ol/source/setup00_esbuild.sh
@@ -1,6 +1,10 @@
+# create/fill node_modules directory with
+# npm install
+
 esbuild \
     --bundle \
     --minify \
     --sourcemap \
     --target=chrome101 \
+    --outfile=out.js \
     main.js
